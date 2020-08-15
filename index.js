48ca1b25e5e3d67fe29e6a0153900dd569639be2
Boyu <rider945@outlook.com>
2020-08-15 21:28:13 +0800

    feature4: done

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function(){
     e.preventDefault();
 
     let formData =  {
+                      'uuid': generateUUID(),
                       'date': form.date.value,
                       'category': form.category.value,
                       'description': form.description.value,
@@ -20,17 +21,30 @@ document.addEventListener("DOMContentLoaded", function(){
     updateRecords();
     console.log(recordList);
     showRecords(recordList);
-
-    function updateRecords() {
-      localStorage.setItem('records',JSON.stringify(recordList))
-    }
   })
+
+
 
   function showRecords(data){
     panel.innerHTML = ''
     data.reverse().map((el)=>{
       panel.innerHTML += recordsHTML(el)
     });
+    const removeBtns = document.querySelectorAll('.remove')
+
+    removeBtns.forEach((el)=>{el.addEventListener('click', removeHandler)})
+  }
+
+  function removeHandler(evt){
+    let remove = recordList.find((record)=>{ return record['uuid'] === evt.target.dataset.id})
+
+    recordList.splice(recordList.indexOf(remove), 1)
+    updateRecords();
+    showRecords(recordList);
+  }
+
+  function updateRecords() {
+    localStorage.setItem('records',JSON.stringify(recordList))
   }
 
   function recordsHTML(data) {
@@ -40,9 +54,19 @@ document.addEventListener("DOMContentLoaded", function(){
             <td>${data.category}</td>
             <td>${data.description}</td>
             <td>${data.amount}</td>
-            <td><span class="remove">x</span></td>
+            <td><span class="remove" data-id=${data.uuid}>x</span></td>
             </tr>
            `
   }
+
+  function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (d+ Math.random()*16)%16 | 0;
+      d = Math.floor(d/16);
+      return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+  };
 
 }); 
